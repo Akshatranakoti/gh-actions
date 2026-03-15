738726489fbe0e036450640b5825b9dfdc2fef07
Akshat03 <akshatranakoti03@gmail.com>
2026-03-15 11:27:45 +0530

    added comment

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import './index.css';
-//This is important
+//This is very imp
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
